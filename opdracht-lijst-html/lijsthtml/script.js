45b7252db531ab94567a3760c08550a1b20a02a8
Karim Dehbi <karim.dehbi@gmail.com>
2022-03-10 11:46:41 +0100

    start lijst html

--- opdracht-lijst-html/lijsthtml/script.js
+++ opdracht-lijst-html/lijsthtml/script.js
@@ -1,18 +1,33 @@
-// An unordered list that displays an object content.
-let createList = function (arr) {
-  let list = document.createElement("ul");
+let objectVoorbeeld1 = { eigenSchap: "hallo", soort: "object 1" };
+let objectVoorbeeld2 = { eigenSchap: "hi", soort: "object 2" };
+let objectVoorbeeld3 = { eigenSchap: "hey", soort: "object 3" };
+let lijstObjecten = [objectVoorbeeld1, objectVoorbeeld2, objectVoorbeeld3];
+// print hier aantal elementen uit in je array
+console.log(lijstObjecten.length);
 
-  for (let key in arr) {
+// loop, die evenveel keer wordt uitgeprint als elementen in je lijst
+// print het eerste element van de lijst uit
+// pas de loop aan zodat je elk element 1 keer uitprint
+for (let i = 0; i < lijstObjecten.length; i++) {
+    objectPrinten(lijstObjecten[i]);
+}
+
+// schrijf een functie die elke eigenschap/property van je object zal uitprinten
+// vb.
+function objectPrinten(parameter) {
+    console.log(parameter.eigenSchap + ' ' + parameter.soort);
+}
+
+// An unordered list that displays an object content.
+function createList (lijst) {
+  let unorderedList = document.createElement("ul");
+
+  for (let parameter in lijst) {
     let listItem = document.createElement("li");
-    listItem.textContent = `${key} -------- ${arr[key]}`;
-    list.appendChild(listItem);
+    listItem.textContent = objectPrinten(parameter);
+    unorderedList.appendChild(listItem);
   }
-  return list;
+  return unorderedList;
 };
 
-for (let person of actors) {
-  let newHeader = document.createElement("h3");
-  newHeader.textContent = person["name"];
-  container.appendChild(newHeader);
-  container.appendChild(createList(person));
-}
+createList(lijstObjecten);
